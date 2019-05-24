ba3dd08ae5c681f8d1e9e10a6865da930e05ca83
Moritz Bührer <30311027+moritzbuehrer@users.noreply.github.com>
2019-05-24 11:15:19 +0200

    Initial python files

--- startSensors.py
+++ startSensors.py
@@ -1,5 +1,15 @@
+import paho.mqtt.client as mqtt
+import ssl
 import multiprocessing as mp
 
+#Settings
+broker_address = "mqtt.iot-embedded.de"
+port = 8883
+username = "transport"
+password = "{Kaputt}"
+
+
+#Sensor functions
 def read_temp():
     print("Temp")
 
@@ -13,8 +23,26 @@ def read_preassure():
     print("preassure")
 
 
+#MQTT components
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+
 
 if __name__ == '__main__':
+
+    #Initialize MQTT Client
+    client = mqtt.Client("Sensors")  # create new instance
+    client.on_connect = on_connect
+    client.tls_set(ca_certs=None, certfile=None, keyfile=None, cert_reqs=ssl.CERT_REQUIRED,
+                   tls_version=ssl.PROTOCOL_TLS, ciphers=None)
+    client.username_pw_set(username, password)
+
+    #Connect Client
+    client.connect(broker_address, port)
+    client.loop_start()
+
+    #Start fetching sensor data
     output = mp.Queue()
 
     processes = [mp.Process(target=read_temp()),
